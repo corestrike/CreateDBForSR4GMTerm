78009146c477984f3e54de4025e9a6f486438bc5
corestrike <corestrike@gmail.com>
2012-10-20 16:19:29 +0900

    整理

--- CreateDatabaseForSR4GMTerm/SqliteDB.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#define DBNAME @"sr4gmterm.sqlite"
-#define DBPATH @"sr4gmtermdata.sqlite"
-#define DBFLAG @"dbflag"
-
-@interface SqliteDB : NSObject
-- (void) initializeDatabaseIfNeeded;
-+ (NSString*) getDatabasePath;
-@end
