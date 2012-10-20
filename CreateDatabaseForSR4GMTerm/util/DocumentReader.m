98a05e37e20f86ace0377153e4a0b3606ae25033
corestrike <corestrike@gmail.com>
2012-10-20 16:20:44 +0900

    整理

---
+++ CreateDatabaseForSR4GMTerm/util/DocumentReader.m
@@ -0,0 +1,78 @@
+//
+//  DocumentReader.m
+//  SR4GMManageTerminal
+//
+//  Created by corestrike on 2012/10/05.
+//
+//
+
+#import "DocumentReader.h"
+#import "FMDatabase.h"
+#import "SqliteDB.h"
+#import "FileReader.h"
+
+@implementation DocumentReader
++ (void)insertInitialData
+{
+    NSString* dbPath = [SqliteDB getDatabasePath];
+    FMDatabase* db = [FMDatabase databaseWithPath:dbPath];
+
+    if(![db open]) {
+        return;
+    }
+    [db setShouldCacheStatements:YES];
+
+    FMResultSet *tableExistRS = [db executeQuery:@"SELECT name FROM sqlite_master WHERE type='table' AND name='reference';"];
+    if (NO == [tableExistRS next]) {
+        [db beginTransaction];
+        BOOL tableCreated = [db executeUpdate:@"CREATE VIRTUAL TABLE reference USING fts4(category INTEGER, word TEXT, jword TEXT, page TEXT, jpage TEXT);"];
+        if (tableCreated == NO) {
+            NSLog(@"*** Failed: %d (%@)", [db lastErrorCode], [db lastErrorMessage]);
+            return;
+        }else{
+            NSLog(@"OK, Virtual Table created.");
+        }
+        [db commit];
+    }
+
+    FMResultSet *existRecords = [db executeQuery:@"SELECT * FROM reference WHERE rowid = 1;"];
+    if (NO == [existRecords next]) {
+        [db beginTransaction];
+        FileReader *txtFileReader = [[FileReader alloc] initWithFilePath:[[NSBundle mainBundle] pathForResource:@"sr4data_utf8" ofType:@"txt"]];
+        __block int readLines = 0;
+        [txtFileReader enumerateLinesUsingBlock:^(NSString *aNewLine, BOOL *stop) {
+            if (readLines >= 1353) {
+                // Drop the last 18 lines.
+                //      [self.db commit];
+                FMResultSet *result = [db executeQuery:@"SELECT count(*) from reference;"];
+                if ([result next]) {
+                    NSLog(@"Result: imported %d records", [result intForColumnIndex:0]);
+                }else{
+                    NSLog(@"Result: finished importing, but has no data.");
+                }
+                *stop = YES;
+            }
+            @autoreleasepool {
+                NSArray *arr = [aNewLine componentsSeparatedByString:@"|"];
+                NSNumber *category = [NSNumber numberWithInt:[[arr objectAtIndex:0] intValue]];
+                NSString *word = [arr objectAtIndex:1];
+                NSString *jword = [arr objectAtIndex:2];
+                NSString *page = [arr objectAtIndex:3];
+                NSString *jpage = [arr objectAtIndex:4];
+//                NSLog(@"%@, %@, %@, %@, %@", category, word, jword, page, jpage);
+                [db executeUpdate:@"INSERT INTO reference(category, word, jword, page, jpage) VALUES(?, ?, ?, ?, ?);", category, word, jword, page, jpage];
+                if (readLines % 100 == 0) {
+                    NSLog(@"Reaches line: %d", readLines);
+                }                
+            }
+            readLines++;
+        }];
+        [db commit];
+        [db close];
+    }else{
+        FMResultSet *numberOfRecords = [db executeQuery:@"SELECT count(*) FROM reference;"];
+        NSLog(@"Already has %d records.", [numberOfRecords intForColumnIndex:0]);
+        [db close];
+    }
+}
+@end
